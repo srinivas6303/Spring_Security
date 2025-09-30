dbf1cff0f28b8df7722a32ef03e28d527fbf395d
Srinvas-dappu <srinivas64400@gmail.com>
2025-09-30 21:19:24 +0530

    added sessesionBasedAuth

--- src/main/java/com/example/Jwt_demo/SpringSecurityApplication.java
+++ src/main/java/com/example/Jwt_demo/SpringSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtDemoApplication {
+public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtDemoApplication.class, args);
+		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
 }
